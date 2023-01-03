881f51ba42e77c1bbb7a2490d841ddbbc5e7f3d6
Gustavo Tonolli <gustavo.tonolli@hotmail.com>
2023-01-03 18:35:57 +0000

    tips

---
+++ relationalOperators.js
@@ -0,0 +1,19 @@
+// relational operators;
+// its an expression that returns true or false;
+/* 
+=== strictly equal to = considers the data type too;
+== equal to , doesn't consider data type;
+> greater than;
+< less than;
+>= greater than or equal to;
+<= less than or equal to;
+!= not equal to;
+!== strictly not equal to;
+! not operator;
+*/
+
+let num1 = 5;
+let num2 = 3;
+let num3 = 10;
+
+console.log(!(num1 => num2));
---
+++ tips.js
@@ -0,0 +1,5 @@
+// some tips to follow along my journet
+
+// to use the tag script in html we can use the defer atribute, so we don't need to put script tags in the end of the html file. Instead we can still put in the head, and it will work normally.
+
+// Example:  <script src="./tips.js" defer></script>
