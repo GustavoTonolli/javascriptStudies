c5a4bef42421835c4d2f30f37f22a3ebdaccc325
Gustavo Tonolli <gustavo.tonolli@hotmail.com>
2023-01-03 20:11:37 +0000

    added typeofOperator

---
+++ typeofOperator.js
@@ -0,0 +1,13 @@
+// typeof operator example
+
+let a, b, c, d;
+
+a = "some data";
+b = 15;
+c = true;
+d = [1, 2, 3];
+
+console.log(typeof(a));
+console.log(typeof(b));
+console.log(typeof(c));
+console.log(typeof(d));
