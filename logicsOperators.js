6741f4ef80d023a020bbc6b3c1a287a7662e613d
Gustavo Tonolli <gustavo.tonolli@hotmail.com>
2023-01-03 19:16:17 +0000

    logics

---
+++ logicsOperators.js
@@ -0,0 +1,17 @@
+// && = and;
+// || = or;
+// ! = not;
+
+let n1, n2, n3, n4;
+
+n1 = 2;
+n2 = 4;
+n3 = 5;
+n4 = 3;
+
+if ((n1 >= n3) || (n3 < n4)){
+    console.log("True")
+    
+} else {
+    console.log("False")
+};
