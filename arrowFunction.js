031cc41b7cb1e526acb7115603238974de2f71d2
Gustavo Tonolli <gustavo.tonolli@hotmail.com>
2023-01-04 19:17:29 +0000

    arrowFunctions

---
+++ arrowFunction.js
@@ -0,0 +1,12 @@
+// Arrow Function, Or lambda function
+
+const sum = (v1, v2) => {
+    return v1 + v2
+}
+
+const name = n => { return n } // only 1 parameter no need of paretheses.
+
+const add = i => i += 10
+
+console.log(add(20))
+console.log(sum(5, 10))
