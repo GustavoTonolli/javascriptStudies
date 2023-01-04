809299f33fbd2748397de060aa2a4690be675504
Gustavo Tonolli <gustavo.tonolli@hotmail.com>
2023-01-04 15:11:15 +0000

    functionReturn

--- functions.js
+++ functions.js
@@ -2,12 +2,24 @@
 // a block of commands that I can execute anytime.
 // traditional function functionName(parameters) {code}
 
-function myName() {
+/* function myName() {
     console.log("Gustavo Tonolli")
 }
 
 
 for (let i = 0; i < 10; i++) {
     myName();
+} */
+
+// function with return
+
+function other() {
+    let n1 = 10;
+    let n2 = 15;
+    let res = n1 * n2;
+
+    return res
 }
+let num = other()
+console.log(num)
 
