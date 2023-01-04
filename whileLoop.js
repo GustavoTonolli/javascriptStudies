e4c7e35042d29e7a24fbbebcd8522302268fc4f8
Gustavo Tonolli <gustavo.tonolli@hotmail.com>
2023-01-04 13:19:03 +0000

    loopWhileFatorial

---
+++ whileLoop.js
@@ -0,0 +1,23 @@
+// loop while undefined
+// it tests before de block of code
+
+
+/* let n = 0
+while (n < 10) {
+    console.log(n)
+    n++
+} */
+
+// fatorial
+
+// 5! = 5*4*3*2*1 = 120
+
+let n = 10;
+let fat = 1;
+
+while (n >= 1) {
+    fat *= n
+    n--
+}
+
+console.log(fat)
