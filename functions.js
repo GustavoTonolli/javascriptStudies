bb4cb2ed4ddcdc0550062c994b124f62acf71fd8
Gustavo Tonolli <gustavo.tonolli@hotmail.com>
2023-01-04 14:40:10 +0000

    functions

---
+++ functions.js
@@ -0,0 +1,13 @@
+// functions
+// a block of commands that I can execute anytime.
+// traditional function functionName(parameters) {code}
+
+function myName() {
+    console.log("Gustavo Tonolli")
+}
+
+
+for (let i = 0; i < 10; i++) {
+    myName();
+}
+
