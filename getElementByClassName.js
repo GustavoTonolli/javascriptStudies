d25162eca23ae5d2d449eb1794c20483f2a4c6f0
Gustavo Tonolli <gustavo.tonolli@hotmail.com>
2023-01-05 10:38:42 +0000

    getElementsByClassName

---
+++ getElementByClassName.js
@@ -0,0 +1,13 @@
+const allCourses = [...document.getElementsByClassName("curso")];
+const c1Courses = [...document.getElementsByClassName("c1")];
+const c2Courses = [...document.getElementsByClassName("c2")];
+const specialCourse = document.getElementsByClassName("curso")[1];
+
+c2Courses.map((el) => {
+    el.classList.add("destaque")
+});
+
+specialCourse.innerHTML = "asdas"
+console.log(allCourses);
+console.log(c1Courses);
+console.log(specialCourse);
