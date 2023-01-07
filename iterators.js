b09bd096627cd1acc78c3ea667cdf04921563fc6
Gustavo Tonolli <gustavo.tonolli@hotmail.com>
2023-01-07 21:51:03 +0000

    iterators

---
+++ iterators.js
@@ -0,0 +1,30 @@
+//  Iteradores
+//  Coleções iteraveis :
+// Arrays
+// Strings
+// Map
+// Sets
+//  Iterador = strings[Symbol.iterator]()
+
+
+
+const valores = [10, 8, 9, 2]
+const it_valores = valores[Symbol.iterator]()
+
+const texto = "Youtube"
+const it_texto = texto[Symbol.iterator]()
+
+console.log(texto)
+console.log(it_texto.next())
+console.log(it_texto.next())
+console.log(it_texto.next())
+console.log(it_texto.next())
+console.log(it_texto.next())
+console.log(it_texto.next())
+console.log(it_texto.next())
+console.log(it_texto.next())
+
+//Arrays
+//String
+//Map
+//Sets
