c26b0ffde2910189e211b717c6f1e520df71125a
Gustavo Tonolli <gustavo.tonolli@hotmail.com>
2023-01-07 01:02:46 +0000

    everyMethod

--- findMethod.js
+++ findMethod.js
@@ -1,3 +1,5 @@
+// encontrar um item no array
+
 const p_array = document.querySelector("#array")
 const txt_pesquisar = document.querySelector("#txt_pesquisar")
 const btnPesquisar = document.querySelector("#btnPesquisar")
