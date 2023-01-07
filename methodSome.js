6865f9352f3184e16a1236e36fcfa62804af8976
Gustavo Tonolli <gustavo.tonolli@hotmail.com>
2023-01-07 01:15:17 +0000

    someMethod

--- methodSome.js
+++ methodSome.js
@@ -1,15 +1,14 @@
-// Every verifica o array e procura por equivalencia, todos os elementos precisam ser equivalentes a regra que for estipulada. Every retorna true apenas se todos os itens do array estiverem okay
-
+// Some variacao metodo every = se pelo menos 1 valor do array coincidir array esta conforme = true
 
 const p_array = document.querySelector("#array")
 const btnVerificar = document.querySelector("#btnVerificar")
 const resultado = document.querySelector("#resultado")
 
-const elementos_array = [21, 25, 19, 20, 18, 18, 22]
+const elementos_array = [21, 25, 15, 20, 18, 18, 22]
 p_array.innerHTML = "[" + elementos_array + "]"
 
 btnVerificar.addEventListener("click", (evt) => {
-    const ret = elementos_array.every((e, i) => {
+    const ret = elementos_array.some((e, i) => {
         if (e < 18) {
             resultado.innerHTML = "Array não conforme na posição " + i
         }
