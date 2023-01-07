ca5a78d35374b53a49310d74a1b25eccb9605b8b
Gustavo Tonolli <gustavo.tonolli@hotmail.com>
2023-01-07 21:59:51 +0000

    arrayMethods

---
+++ arrayMethods.js
@@ -0,0 +1,11 @@
+let array = ["CSS"]
+
+
+array.pop() //remove o último item do array
+array.push("html") //adiciona um item no array na última posição.
+array.unshift("html") //adiciona item na posição 0, primeira posição do array
+array.shift() // remove o primeiro do array
+
+array.push("asdassad")
+
+console.log(array)
