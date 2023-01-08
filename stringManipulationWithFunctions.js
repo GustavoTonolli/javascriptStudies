2c866b613ac290c2c695a30b45a1a4ac6a1a1d88
Gustavo Tonolli <gustavo.tonolli@hotmail.com>
2023-01-08 23:24:52 +0000

    stringManipulationFunctions

---
+++ stringManipulationWithFunctions.js
@@ -0,0 +1,10 @@
+// manipulando strings com funcoes
+
+let nome = new String("Gustavo");
+
+console.log(typeof (nome));
+
+console.log(nome.charAt(1));
+console.log(nome.charCodeAt(1));
+console.log(nome.indexOf("G")) // primeiro caractere encontrado
+console.log(nome.lastIndexOf("o")) // ultimo caractere encontrado
