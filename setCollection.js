23486b5de864b55aaab05a590d5db75e3789d806
Gustavo Tonolli <gustavo.tonolli@hotmail.com>
2023-01-08 00:21:33 +0000

    setCollection

---
+++ setCollection.js
@@ -0,0 +1,8 @@
+// colecao set... Nao permite elementos duplicados. nao tem chaves
+
+musicas = new Set(["Musica1"]);
+
+musicas.add("Musica 2");
+
+console.log(musicas)
+// tem os mesmos metodos que map.
