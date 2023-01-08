6edb5c0a045f54e94aca534f8830567649a4b58e
Gustavo Tonolli <gustavo.tonolli@hotmail.com>
2023-01-08 01:07:09 +0000

    objects

---
+++ objects.js
@@ -0,0 +1,35 @@
+// objetos
+
+class Carro {
+    constructor(nome, tipo) {
+        this.nome = nome
+        if (tipo == 1) {
+            this.tipo = "Esportivo"
+        } else if (tipo == 2) {
+            this.tipo = "Luxo"
+        } else {
+            this.tipo = "Utilitario"
+        }
+    }
+    setNome(nome) {
+        this.nome = nome
+    }
+    getNome() {
+        return this.nome
+    }
+    info() {
+        console.log(`Nome : ${this.nome}`)
+        console.log(`Tipo : ${this.tipo}`)
+
+    }
+}
+
+let c1 = new Carro("ford", 1) // new instancia uma nova classe e inicia o construtor
+let c2 = new Carro("Gol", 2)
+let c3 = new Carro("Fiorino", 3)
+c1.setNome("Bmw")
+c1.info()
+
+// criar propriedades key values
+
+
