8a6d175d6ddf66aa75c55bdb985edf454f6e4b0d
Gustavo Tonolli <gustavo.tonolli@hotmail.com>
2023-01-10 02:18:04 +0000

    calculadoraEval

---
+++ calculadora.js
@@ -0,0 +1,65 @@
+const teclasNum = [...document.querySelectorAll(".num")]
+const teclasOp = [...document.querySelectorAll(".op")]
+const teclaRes = document.querySelector(".res")
+const display = document.querySelector(".display")
+const tcopy = document.getElementById("tcopy")
+const tlimpar = document.getElementById("tlimpar")
+const tigual = document.getElementById("tigual")
+
+
+let sinal = false
+let decimal = false
+
+teclasNum.forEach((el) => {
+    el.addEventListener("click", (evt) => {
+        sinal = false
+        if (evt.target.innerHTML == ",") {
+            if (!decimal) {
+                decimal = true
+            } if (display.innerHTML == "0") {
+                display.innerHTML = "0,"
+            } else {
+                display.innerHTML += evt.target.innerHTML
+            }
+
+        } else {
+            if (display.innerHTML == "0") {
+                display.innerHTML = ""
+            }
+            display.innerHTML += evt.target.innerHTML
+        }
+
+
+    });
+});
+teclasOp.forEach((el) => {
+    el.addEventListener("click", (evt) => {
+        if (!sinal) {
+            sinal = true
+            if (display.innerHTML == "0") {
+                display.innerHTML = ""
+            }
+            if (evt.target.innerHTML == "x") {
+                display.innerHTML += "*"
+            } else {
+                display.innerHTML += evt.target.innerHTML
+            }
+        }
+
+    });
+});
+
+tlimpar.addEventListener("click", (evt) => {
+    sinal = false
+    decimal = false
+    display.innerHTML = 0
+})
+tcopy.addEventListener("click", (evt) => {
+
+})
+tigual.addEventListener("click", (evt) => {
+    sinal = false
+    decimal = false
+    const res = eval(display.innerHTML)
+    display.innerHTML = res
+})
