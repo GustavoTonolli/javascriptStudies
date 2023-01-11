443176a29c49d30fefc7765235a3c425d4579448
Gustavo Tonolli <gustavo.tonolli@hotmail.com>
2023-01-11 01:43:16 +0000

    mathLibrary

---
+++ math.js
@@ -0,0 +1,9 @@
+// funcoes math
+
+const randomNum = Math.random() * 10; // retorna um numero aleatorio entre 1 a 10
+const randomInt = Math.floor(randomNum); // retorna apenas o numero inteiro
+const randomArrend = Math.round(randomNum) // arrendonda um numero
+
+
+Math.pow(10, 2) // potencia, base, potencia. 10 elevado a 2
+console.log(Math.sqrt(25)) // retorna a raiz quadrada
