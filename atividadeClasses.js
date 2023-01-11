41bd4c63b24c71d76f0be68ab5710095d45efbf6
Gustavo Tonolli <gustavo.tonolli@hotmail.com>
2023-01-11 05:16:17 +0000

    classActBalls

--- atividadeClasses.js
+++ atividadeClasses.js
@@ -10,13 +10,48 @@ let bolas = [];
 let numBola = 0;
 
 class Bola {
-    constructor() {
+    constructor(arrayBolas, palco) {
         this.tam = Math.floor(Math.random() * 10); + 10 // gera tamanho aleatorio para a bolinha
         this.r = Math.floor(Math.random() * 255); // gera cor aleatoria rgb
         this.g = Math.floor(Math.random() * 255);// gera cor aleatoria rgb
         this.b = Math.floor(Math.random() * 255); // gera cor aleatoria rgb
         this.px = Math.floor(Math.random() * (larguraPalco - this.tam)); // posicao aleatoria nascimento da bolinha
         this.py = Math.floor(Math.random() * (alturaPalco - this.tam)); // posicao aleatoria nascimento da bolinha
+        this.velx = Math.floor(Math.random() * 2) + 0.5; // velocidade aleatoria
+        this.vely = Math.floor(Math.random() * 2) + 0.5; // velocidade aleatoria
+        this.dirx = (Math.random() * 10) < 5 ? 1 : -1;
+        this.diry = (Math.random() * 10) < 5 ? 1 : -1;
+        this.palco = palco;
+        this.arrayBolas = arrayBolas;
+        this.id = Date.now() + "_" + Math.random() * 9999999999999999999;
+        this.desenhar();
+        this.controle = setInterval(this.controlar, 10);
+        this.eu = document.getElementById(this.id);
+        numBola++;
+        num_objetos.innerHTML = numBola;
+    }
+    minhaPos = () => {
+        return this.arrayBolas.indexOf(this)
+    }
+    remover = () => {
+        clearInterval(this.controle);
+        bolas = bolas.filter((b) => {
+            if (b.id != this.id)
+                return b;
+        });
+        this.eu.remove();
+        numBola--
+        num_objetos.innerHTML = numBola;
+    }
+    desenhar = () => {
+        const div = document.createElement('div');
+        div.setAttribute('id', this.id);
+        div.setAttribute('class', 'bola');
+        div.setAttribute("style", `left : ${this.px};top:${this.py};width:${this.tam};height:${this.tam};background-color:rgb(${this.r},${this.g},${this.b})`);
+        this.palco.appendChild(div);
+    }
+    controlar = () => {
+
     }
 }
 window.addEventListener("resize", (evt) => { // evento dimensionamento da tela ou palc0
