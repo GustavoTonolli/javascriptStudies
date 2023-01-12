19ec1050e0e039291847e6976ef954d2c891b1e3
Gustavo Tonolli <gustavo.tonolli@hotmail.com>
2023-01-12 02:03:48 +0000

    modulosJavaScript

---
+++ cursos.js
@@ -0,0 +1,5 @@
+const cursos = ['Javascript', 'Html', 'CSS', 'JSON', 'NODEJS', 'C#'];
+const carros = ['polo', 'gol', 'fiat']
+
+export { cursos, carros } // exportando cursos e carros para modulo
+// export { carros } // exportando carros para modules
---
+++ modules.js
@@ -0,0 +1,6 @@
+// JavaScript com modulos
+import { cursos } from "./cursos.js"; // importar arquivo de outro arquivo 
+import { carros } from "./cursos.js";
+
+console.log(carros);
+
