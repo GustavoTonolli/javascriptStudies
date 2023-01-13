e329eb33d4ffc65c74b21cabd2a313919403f6eb
Gustavo Tonolli <gustavo.tonolli@hotmail.com>
2023-01-13 00:54:24 +0000

    moreOfStringFunctionsObj

--- stringManipulationWithFunctions.js
+++ stringManipulationWithFunctions.js
@@ -1,6 +1,8 @@
-// manipulando strings com funcoes
+// manipulando obj strings com funcoes
 
-let nome = new String("Gustavo");
+let nome = new String("Gustavo Tonolli");  // objts
+let nom1e = new String("Gustavo");
+
 
 console.log(typeof (nome));
 
@@ -8,3 +10,12 @@ console.log(nome.charAt(1));
 console.log(nome.charCodeAt(1));
 console.log(nome.indexOf("G")) // primeiro caractere encontrado
 console.log(nome.lastIndexOf("o")) // ultimo caractere encontrado
+
+console.log(nome.localeCompare(nom1e)); // comparar strings, 0 = strings iguais, 1 = strings diferentes 1 string maior, -1 = 2 string maior'
+
+// console.log(nome.replace("G", "A")); // substituir caracteres G pelo A, so substitui o primeiro encontrado
+console.log(nome.search("Tonolli"))
+console.log(nome.slice(8, 15)) // retorna os caracteres da posicao 8 ate 15
+arr = nome.split(" "); // faz um recorte na string onde for indicado e retorna um array com cada parte repartida
+console.log(arr);
+console.log(arr.length);    
